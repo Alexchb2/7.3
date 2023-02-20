fab13901aa30998f865337c387f57dfc4a1e4dfb
alexc <alexchb.makarenko@yandex.ru>
2023-02-20 21:26:43 +0300

    playwright

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,11 +1,9 @@
 const { test, expect } = require("@playwright/test");
 const { chromium } = require("playwright");
-const {email, password, incorrectEmail, incorrectPassport,} = require("./user");
+const {email, password, invalidEmail, invalidPassport,} = require("./user.js");
 
 test("Successful authorization", async () => {
   const browser = await chromium.launch({
-    // headless: false,
-    // slowMo: 500,
   });
   
   const page = await browser.newPage("https://netology.ru/?modal=sign_in");
@@ -14,22 +12,18 @@ test("Successful authorization", async () => {
   await page.fill('[placeholder="Пароль"]', password);
   await page.click('[data-testid="login-submit-btn"]');
   await expect(page.locator("h2")).toContainText(["Мои курсы и профессии"]);
-  await page.screenshot({ path: "screenshotSuccessful.png", fullPage: true });
   browser.close();
 }, 80000);
 
 test("Failed authorization", async () => {
   const browser = await chromium.launch({
-    // headless: false,
-    // slowMo: 500,
   });
   const page = await browser.newPage("https://netology.ru/?modal=sign_in");
   await page.goto("https://netology.ru/?modal=sign_in");
-  await page.fill('[placeholder="Email"]', incorrectEmail);
-  await page.fill('[placeholder="Пароль"]', incorrectPassport);
+  await page.fill('[placeholder="Email"]', invalidEmail);
+  await page.fill('[placeholder="Пароль"]', invalidPassport);
   await page.click('[data-testid="login-submit-btn"]');
   const error = await page.locator('[data-testid="login-error-hint"]');
   await expect(error).toHaveText("Вы ввели неправильно логин или пароль");
-  await page.screenshot({ path: "screenshotFailed.png", fullPage: true });
   browser.close();
 }, 80000);
